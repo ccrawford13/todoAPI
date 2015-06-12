ae4e4950c9d098effbe81da5852b875be9871ef4
Chad Crawford <ccrawforduwlax@gmail.com>
2015-06-11 20:48:51 -0500

    Update Items & toggle completed status
    Update Items from controller - toggle completed status

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ActiveRecord::Base
   validates_presence_of :list
   validates_presence_of :title
   validates_length_of :title, minimum: 5
+
+  def toggle_completed
+    if self.completed == false
+      self.update_attributes(completed: true)
+    else
+      self.update_attributes(completed: false)
+    end
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,4 +5,29 @@ RSpec.describe Item, type: :model do
   it { should validate_presence_of :list }
   it { should validate_presence_of :title }
   it { should validate_length_of(:title).is_at_least(5) }
+
+  describe '#toggle_completed' do
+
+    let(:item) { create(:item) }
+
+    context 'when item is incomplete' do
+
+      it 'marks item as completed' do
+        item.toggle_completed
+        expect(item.completed).to eq true
+      end
+    end
+
+    context 'when item is completed' do
+
+      before do
+        item.update_attributes(completed: true)
+      end
+
+      it 'marks item as incomplete' do
+        item.toggle_completed
+        expect(item.completed).to eq false
+      end
+    end
+  end
 end
