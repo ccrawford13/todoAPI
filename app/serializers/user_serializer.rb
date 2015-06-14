8f34f5daefeef5a08109f702d6c3e578b6ee90b4
Chad Crawford <ccrawforduwlax@gmail.com>
2015-06-13 20:18:54 -0500

    Add json-schema gem -> to test serializers and valid son output

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :user_name, :email, :created_at
+  attributes :id, :full_name, :user_name, :email, :creation_date
 
   def full_name
     object.full_name
   end
-
-  def created_at
-    object.created_at.strftime('%B %d, %Y')
-  end
-
 end
